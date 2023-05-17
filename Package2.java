bd37097204b68d81d3574a47ecbd599740e46246
SaraAlajmy <saraalagami@gmail.com>
2023-05-17 20:07:45 +0300

    arrays

--- Package2.java
+++ Package2.java
@@ -1,6 +1,12 @@
 public class Package2 {
 
+    private short[] instructionMemory = new short[1024];
+    private byte[] dataMemory = new byte[2048];
+    private byte[] registers = new byte[66];
+
     public void fetch(){
+// read from a txt file?
+        
 
     }
 
