38230653c36151d0845c298dd1c31a043feb3945
SaraAlajmy <saraalagami@gmail.com>
2023-05-17 20:26:14 +0300

    abunchofadditions

--- Package2.java
+++ Package2.java
@@ -1,16 +1,22 @@
 public class Package2 {
 
-    private short[] instructionMemory = new short[1024];
-    private byte[] dataMemory = new byte[2048];
-    private byte[] registers = new byte[66];
+    private static short[] instructionMemory = new short[1024];
+    private static byte[] dataMemory = new byte[2048];
+    private static byte[] registers = new byte[66];
+    private int pc = 0;
 
     public void fetch(){
 // read from a txt file?
-        
+
+        for(int i = 0; i < instructionMemory.length; i++) {
+            short instruction = instructionMemory[pc];
+            decode(instruction);
+            pc++;
+        }
 
     }
 
-    public void decode(){
+    public void decode(short instruction){
 
     }
 
