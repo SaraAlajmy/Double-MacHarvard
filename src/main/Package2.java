32d0a1e183f656b0675ea403308312d00d27ddc9
SaraAlajmy <saraalagami@gmail.com>
2023-05-18 18:12:26 +0300

    pipeliningComplete

--- src/main/Package2.java
+++ src/main/Package2.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Vector;
 
 public class Package2 {
     private static short[] instructionMemory = new short[1024];
@@ -16,21 +17,21 @@ public class Package2 {
     private static int starting = 0;
 
     public static void pipeline(){
-        short instruction = 0;
-        byte[] values = {0,0,0,0,0};
+        short[] instruction = {0,0};
+        Vector<Object> values = new Vector<>();
         for(int cycle = 0; cycle<instructionMemory.length+2; cycle++, starting++){
-            short tempInstruction =0;
-            byte[] tempValues = {0,0,0,0,0};
+            short[] tempInstruction = {0,0};
+            Vector<Object> tempValues = new Vector<>();
             if(cycle<instructionMemory.length)
                 tempInstruction = fetch();
             if(cycle<instructionMemory.length+1 && starting>0)
                 tempValues = decode(instruction);
             if(starting>1)
-                execute(values[0], values[1], values[2], values[3], values[4]);
+                execute(((byte[])tempValues.get(0))[0], ((byte[])tempValues.get(0))[1], ((byte[])tempValues.get(0))[2], ((byte[])tempValues.get(0))[3], ((byte[])tempValues.get(0))[4], (short)tempValues.get(1));
             instruction = tempInstruction; values = tempValues;
         }
     }
-    public static short fetch(){
+    public static short[] fetch(){
 // read from a txt file?
         try {
             BufferedReader br = new BufferedReader(new FileReader("input.txt"));
@@ -50,33 +51,36 @@ public class Package2 {
 //        for(int i = 0; i < instructionMemory.length; i++) {
             short instruction = instructionMemory[pc];
             pc++;
-            return instruction;
+            return new short[]{instruction, pc};
 //            decode(instruction);
 //        }
 
     }
 
-    public static byte[] decode(short instruction){
-        byte opcode = (byte) ((instruction & 0b1111000000000000) >>> 12);  // bits 15:12
-        byte R1 = (byte) ((instruction & 0b0000111111000000) >>> 6); // 11:6
+    public static Vector decode(short[] instruction){
+        byte opcode = (byte) ((instruction[0] & 0b1111000000000000) >>> 12);  // bits 15:12
+        byte R1 = (byte) ((instruction[0] & 0b0000111111000000) >>> 6); // 11:6
         byte inR1 = registers[R1];
-        byte R2orImm= (byte) (instruction & 0b0000000000111111); //6:0
+        byte R2orImm= (byte) (instruction[0] & 0b0000000000111111); //6:0
         byte inR2 = registers[R2orImm];
-        return new byte[]{opcode,R1, R2orImm, inR1, inR2};
+        Vector<Object> result = new Vector<>();
+        result.add(new byte[]{opcode, R1, R2orImm, inR1, inR2});
+        result.add(instruction[1]);
+        return result;
         // excute(opcode,R1, R2orImm, inR1, inR2);
     }
 
-    public static void execute(byte opcode, short r1 ,short r2orImm, byte inR1, byte inR2){
+    public static void execute(byte opcode, short r1 ,short r2orImm, byte inR1, byte inR2, short pcOld){
         SREG = 0;
         switch (opcode){
             case 0: registers[r1] = add(inR1, inR2);break;
             case 1: registers[r1] = sub(inR1, inR2);break;
             case 2: registers[r1]=(byte)(inR1*inR2); updateNegAndZero(registers[r1]);break;
             case 3: registers[r1]=(byte)r2orImm;break;
-            case 4: pc=(inR1==0)?(byte) (pc+r2orImm):pc;break;
+            case 4: if(inR1==0){pc = (byte) (pcOld+r2orImm);starting = 0;}break;
             case 5: registers[r1] = (byte)(inR1 & inR2); updateNegAndZero(registers[r1]);break;
             case 6: registers[r1] = (byte)(inR1 | inR2); updateNegAndZero(registers[r1]);break;
-            case 7: pc = concatenate(inR1, inR2);break;
+            case 7: pc = concatenate(inR1, inR2); starting =0; break;
             case 8: registers[r1] = (byte)(((inR1 & 0xFF)<<r2orImm) | ((inR1 & 0xFF)>>>(8- r2orImm))); updateNegAndZero(registers[r1]);break;
             case 9: registers[r1] = (byte)(((inR1 & 0xFF)>>> r2orImm) | ((inR1 & 0xFF)<<(8- r2orImm))); updateNegAndZero(registers[r1]);break;
             case 10: registers[r1]=  dataMemory[r2orImm] ;break;
